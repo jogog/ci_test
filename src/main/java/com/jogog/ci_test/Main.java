50cd834141b8bad0fec990a53f78c3b581a3b9ea
jogog <jogog007@mail.ru>
2021-02-12 17:19:48 +0300

    still testing

--- src/main/java/com/jogog/ci_test/Main.java
+++ src/main/java/com/jogog/ci_test/Main.java
@@ -3,5 +3,6 @@ package com.jogog.ci_test;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Let's put smth new");
     }
 }
