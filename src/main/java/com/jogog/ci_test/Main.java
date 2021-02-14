1e87682487c6ecdcb1d499c4dd53bd67b7e7a779
jogog <jogog007@mail.ru>
2021-02-15 00:06:34 +0300

    hmm

--- src/main/java/com/jogog/ci_test/Main.java
+++ src/main/java/com/jogog/ci_test/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Let's put smth new");
         System.out.println("Yuhuuu");
+	System.out.println("Yuh");
     }
 }
