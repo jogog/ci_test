d46288181f6ff61dab14be54f986f7dad30fc005
jogog <jogog007@mail.ru>
2021-02-19 18:49:50 +0300

    trying with failing test

---
+++ src/test/java/com/jogog/ci_test/HelloControllerTest.java
@@ -0,0 +1,13 @@
+package com.jogog.ci_test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloControllerTest {
+
+    @Test
+    void index() {
+        assertEquals(2,  1);
+    }
+}
